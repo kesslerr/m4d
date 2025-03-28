f7c944508f131210b868fb1f0ee2314b33c152de
Roman Kessler <rkesslerx@gmail.com>
2025-03-28 08:51:13 +0100

    R packages update

--- env/export_R_env.R
+++ env/export_R_env.R
@@ -1,6 +1,14 @@
 # save the current R environment to file
 
-R.version.string
+# navigate to the R env
+setwd("../env")
 
+Rversion <- R.version.string
+# save version string to txt file
+write(Rversion, "Rversion.txt")
+
+# make sure to have sourced the activate.R file before running
+
+# save the installed packages to a csv file
 installed_packages <- as.data.frame(installed.packages()[, c("Package", "Version")])
-write.csv(installed_packages, "installed_packages.csv", row.names = FALSE)
+write.csv(installed_packages, "R_packages.csv", row.names = FALSE)
